d58c7d400b149ab95a33f3e169bc96134b926dd4
Boss-of-Trees <87606790+Boss-of-Trees@users.noreply.github.com>
2021-07-22 13:22:46 +0200

    SocketDict

---
+++ src/dev/beni/utils/NotDictButDict.java
@@ -0,0 +1,24 @@
+package dev.beni.utils;
+
+import java.util.*;
+
+public class NotDictButDict {
+
+    private List<Integer> keys = new ArrayList<Integer>();
+    private List<String> value = new ArrayList<String>();
+
+    public NotDictButDict() {
+
+    }
+
+    public String toString() {
+        String stringified;
+        stringified = "Hello nothing to see here rn";
+        return stringified;
+    }
+
+    public List fromString() {
+        return keys;
+    }
+
+}
---
+++ src/dev/beni/utils/SocketDict.java
@@ -0,0 +1,24 @@
+package dev.beni.utils;
+
+import java.util.*;
+
+public class SocketDict {
+
+    private List<String> keys = new ArrayList<String>();
+    private List<String> value = new ArrayList<String>();
+
+    public SocketDict() {
+
+    }
+
+    public String toString() {
+        String stringified;
+        stringified = "Hello nothing to see here rn, sry";
+        return stringified;
+    }
+
+    public List fromString() {
+        return keys;
+    }
+
+}
